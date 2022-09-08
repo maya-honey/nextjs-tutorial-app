d925cc92583ec13b9551b05cce53401ad1c79796
maya-honey <sasaki123.321ikasas@gmail.com>
2022-09-08 18:51:36 +0900

    pre-rendering start

--- NEXTJS/hello-world/pages/index.js
+++ NEXTJS/hello-world/pages/index.js
@@ -1,6 +1,11 @@
 import Link from 'next/link'
-
+import {useRouter} from 'next/router'
 function Home() {
+    const router = useRouter()
+
+    const handleClick = () => {
+        router.push('/product')
+    }
     return (
         <div>
             <h1>Home Page</h1>
@@ -11,6 +16,7 @@ function Home() {
                     </Link>
                 </li>
             </ul>
+            <button onClick={handleClick} >Button to Product</button>
         </div>
     )
 }
