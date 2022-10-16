6a8d369d59f9fb4a6c4fc06bb7709f9d0b2674f5
maya-honey <sasaki123.321ikasas@gmail.com>
2022-10-16 15:00:33 +0900

    end pre-rendering

--- NEXTJS/pre-rendering/pages/events.js
+++ NEXTJS/pre-rendering/pages/events.js
@@ -1,39 +1,24 @@
-import {useState} from 'react'
-import Link from 'next/link'
+import {useEffect, useState} from 'react'
+import {useRouter} from 'next/router'
 
 function EventList({eventList}){
     const [events, setEvents] = useState(eventList)
+    const router = useRouter()
 
     const fetchEvents = async (e) => {
-        let eventName = e.currentTarget.dataset['index']
+        const eventName = e.currentTarget.dataset['index']
         const response = await fetch(`http://localhost:3001/events?category=${eventName}`)
         const data = await response.json()
         setEvents(data)
+        router.push(`/events?category=${eventName}`, undefined, { shallow: true})
     }
 
     return(
         <>
-            <Link href="/events?vategory=sports">
-                <a>
-                    <span data-index="sports" onClick={fetchEvents}>Sports Events / </span>
-                </a>
-            </Link>
-            <Link href="/events?vategory=technology">
-                <a>
-                <span data-index="technology" onClick={fetchEvents}>Technology Events / </span>
-                </a>
-            </Link>
-            <Link href="/events?vategory=food">
-                <a>
-                    <span data-index="food" onClick={fetchEvents}>Food Events / </span>
-                </a>
-            </Link>
-            <Link href="/events?vategory=art">
-                <a>
-                    <span data-index="art" onClick={fetchEvents}>Art Events / </span>
-                </a>
-            </Link>
-            
+            <button data-index="sports" onClick={fetchEvents}>Sports Events</button>
+            <button data-index="technology" onClick={fetchEvents}>Technology Events</button>
+            <button data-index="food" onClick={fetchEvents}>Food Events</button>
+            <button data-index="art" onClick={fetchEvents}>Art Events</button>
             <h1>List of events</h1>
             {
                 events.map(event => {
